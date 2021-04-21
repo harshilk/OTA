b8ccfcd1fe4b0310c263b2558b7626145e4abd4a
Blaster4385 <53873108+Blaster4385@users.noreply.github.com>
2021-04-21 15:33:22 +0530

    Create RMX1851.mk

---
+++ RMX1851/RMX1851.mk
@@ -0,0 +1,16 @@
+{
+  "error": false,
+  "id": "3000e3c2d716037257e395a130e6641b407e1a190458097cfc546534c8665484",
+  "filename": "PixelBlaster_1.0_RMX1851-11.0-20210421-0941-OFFICIAL.zip",
+  "datetime": 1618998569,
+  "version": "v1.0",
+  "size": 1296570721,
+  "url": "https://github.com/Blaster4385/build_aosp/releases/download/v4/PixelBlaster_1.0_RMX1851-11.0-20210421-0941-OFFICIAL.zip",
+  "filehash": "edd1ae3dea1d98b22c87474f9bd37e03",
+  "website_url": "",
+  "news_url": "https://t.me/TenX_Updates",
+  "donate_url": "",
+  "maintainer": "TheTablaster",
+  "maintainer_url": "https://t.me/V3Nk4135H",
+  "forum_url": ""
+}
